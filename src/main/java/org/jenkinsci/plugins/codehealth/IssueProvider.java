b6496ee0f8b730aee5aa3440bf2c87b058228fb6
Michael Prankl <eidottermihi@gmail.com>
2015-06-09 13:06:27 +0200

    Javadoc for extension point "IssueProvider".

--- src/main/java/org/jenkinsci/plugins/codehealth/IssueProvider.java
+++ src/main/java/org/jenkinsci/plugins/codehealth/IssueProvider.java
@@ -9,6 +9,15 @@ import org.jenkinsci.plugins.codehealth.model.Issue;
 import java.util.Collection;
 
 /**
+ * Extension point for code analysis plugins which want to contribute issues for continuous code quality monitoring.
+ * An issue must have set following attributes:
+ * <ul>
+ *     <li>origin - can refer to plugin id</li>
+ *     <li>contextHashCode - must be unique for the scope of the plugin (origin)</li>
+ *     <li>priority</li>
+ *     <li>message</li>
+ * </ul>
+ *
  * @author Michael Prankl
  */
 public abstract class IssueProvider implements ExtensionPoint {
