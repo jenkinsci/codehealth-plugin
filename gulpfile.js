e7d15d6808c8b3323cb9924282e4d73a59a70e0e
Michael Prankl <eidottermihi@gmail.com>
2015-10-19 17:37:58 +0200

    Dont need JQuery UI for Bootstrap Accordions.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ builder.defineTasks(['test','bundle','rebundle']);
 builder.bundle('src/main/js/codehealth.js')
     .withExternalModuleMapping('jquery-detached', 'jquery-detached:jquery2')
     .withExternalModuleMapping('bootstrap-detached', 'bootstrap:bootstrap3')
-    .withExternalModuleMapping('jqueryui-detached', 'jquery-detached:jqueryui1')
     .inDir('src/main/webapp/bundles');
 
 
