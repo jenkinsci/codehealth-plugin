bee335ad78af3246ddddd7d968d0725dbe4de0b7
Michael Prankl <eidottermihi@gmail.com>
2015-10-19 16:25:20 +0200

    Added simple table which shows all issues. The issues are requested via an AJAX call to the JSON issues API.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ builder.defineTasks(['test','bundle','rebundle']);
 
 builder.bundle('src/main/js/codehealth.js')
     .withExternalModuleMapping('jquery-detached', 'jquery-detached:jquery2')
+    .withExternalModuleMapping('bootstrap-detached', 'bootstrap:bootstrap3')
     .inDir('src/main/webapp/bundles');
 
 
--- src/main/js/codehealth.js
+++ src/main/js/codehealth.js
@@ -1,5 +1,22 @@
 var $ = require('jquery-detached').getJQuery();
+var $bootstrap = require('bootstrap-detached').getBootstrap();
 
 var header = $('#header');
 
-window.alert("Hallo Welt!");
+//$('#codehealth_main').html('<h3>JQuery Magic</h3>');
+
+//$bootstrap('[data-toggle="popover"]').popover();
+
+// TODO issueAPI relative url
+var issuesAPI = "http://localhost:8080/jenkins/job/codehealth-local/issues-api/api/json?depth=1";
+$.getJSON(issuesAPI)
+    .done(function (data) {
+        $.each(data.issues, function (i, issue) {
+            var $tr = $("<tr>").append(
+                $("<td>").text(issue.id),
+                $("<td>").text(issue.message),
+                $("<td>").text(issue.priority),
+                $("<td>").text(issue.origin)
+            ).appendTo('#codehealth-issues');
+        });
+    });
