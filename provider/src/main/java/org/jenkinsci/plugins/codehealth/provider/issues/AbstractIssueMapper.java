996f7c21dd4bfde87f2d68fcd8af4ddc79fbf423
Michael Prankl <eidottermihi@gmail.com>
2015-10-26 09:50:06 +0100

    Re-adding AbstractIssueMapper.

---
+++ provider/src/main/java/org/jenkinsci/plugins/codehealth/provider/issues/AbstractIssueMapper.java
@@ -0,0 +1,18 @@
+package org.jenkinsci.plugins.codehealth.util;
+
+import org.jenkinsci.plugins.codehealth.provider.issues.Issue;
+
+/**
+ * Abstract class to support the mapping of annotations/warnings to codehealth Issues.
+ *
+ * @author Michael Prankl
+ */
+public abstract class AbstractIssueMapper<E extends Object> {
+
+    /**
+     * @param e your domain representation of an issue (annotation/warning)
+     * @return the corresponding Issue
+     */
+    public abstract Issue map(E e);
+
+}
