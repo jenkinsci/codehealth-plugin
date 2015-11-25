fd54e1e5ddecd3723c7a27e4a6839694fd086b6f
Michael Prankl <eidottermihi@gmail.com>
2015-11-25 21:36:46 +0100

    WIP Chartist.

--- plugin/src/main/js/graphs/issues.js
+++ plugin/src/main/js/graphs/issues.js
@@ -1,5 +1,5 @@
 var $ = require('jquery-detached').getJQuery();
-var Highcharts = require('highcharts-browserify');
+var Chartist = require('chartist');
 var numeral = require('numeral');
 
 var originsTemplate = require('../handlebars/origins.hbs');
@@ -171,9 +171,20 @@ var updateIssueGraph = function (container, showPie, showTable) {
         parseIssueCountPerOrigin(countPerOriginResponse[0], showPie, showTable);
         issueGraphOptions.chart.renderTo = container;
         // render graph
-        new Highcharts.Chart(
-            issueGraphOptions
-        );
+        var data = {
+            // A labels array that can contain any sort of values
+            labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+            // Our series array that contains series objects or in this case series data arrays
+            series: [
+                [5, 2, 4, 2, 0]
+            ]
+        };
+
+// Create a new line chart object where as first parameter we pass in a selector
+// that is resolving to our chart container element. The Second parameter
+// is the actual data object.
+        console.log(Chartist.version);
+        new Chartist.Line('.chart-container', data);
         console.log("[Issues] Updating finished.");
     });
 };
