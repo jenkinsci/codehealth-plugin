f57bf2c35c49ee2b7efbf6ab1fe73275cd5343cb
Michael Prankl <eidottermihi@gmail.com>
2016-03-17 14:00:39 +0100

    Update JS Libs, add FindBugs to build plugins

--- plugin/gulpfile.js
+++ plugin/gulpfile.js
@@ -4,7 +4,7 @@ builder.defineTasks(['test', 'bundle', 'rebundle']);
 
 builder.bundle('src/main/js/codehealth.js')
     .withExternalModuleMapping('jquery-detached', 'jquery-detached:jquery2')
-    .withExternalModuleMapping('bootstrap-detached', 'bootstrap:bootstrap3')
+    .withExternalModuleMapping('bootstrap-detached', 'bootstrap:bootstrap3', {addDefaultCSS: true})
     .withExternalModuleMapping('handlebars', 'handlebars:handlebars3')
     .withExternalModuleMapping('moment', 'momentjs:momentjs2')
     .withExternalModuleMapping('numeral', 'numeraljs:numeraljs1')
